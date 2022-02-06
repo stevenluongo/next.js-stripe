3be6246bf6280ed7b5443e2704bfb55755cdb029
Steven <murkappco@gmail.com>
2022-02-06 10:28:00 -0500

    downgrade next version / add env config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env : {
+    'NEXT_PUBLIC_STRIPE_PUBLIC_KEY' : process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
+    'STRIPE_SECRET_KEY' : process.env.STRIPE_SECRET_KEY
+  }
 }
