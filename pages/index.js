333dd2a5676aefd325133ac4c00b801c4217c692
Steven <53283472+binolt@users.noreply.github.com>
2022-02-05 22:37:49 -0500

    call payment function in main component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef, forwardRef, useImperativeHandle } from "react"
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import ContactlessIcon from '@mui/icons-material/Contactless';
@@ -16,10 +16,9 @@ import {loadStripe} from '@stripe/stripe-js';
 import { Elements, CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { destroyCookie, parseCookies, setCookie } from "nookies";
 import { fetchPaymentIntent, appendInvoiceItems, finalizeStripeInvoice, createStripeCustomer, createStripeInvoice, calculateShipping, checkoutStorage } from "../lib/stripe";
+import { useRouter } from "next/router";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
-
-
 
 const CssTextField = styled(TextField)({
   '& label.Mui-focused': {
@@ -77,6 +76,7 @@ export default function Index () {
   const [vatTax, setVatTax] = useState(4.07);
   const [subtotal, setSubTotal] = useState(67.99);
   const [total, setTotal] = useState(67.99 + 4.07)
+  const childCompRef = useRef()
 
   const handleInformationSubmit = async() => {
     setMessage(null);
@@ -150,6 +150,11 @@ const handleCheckboxChange = (target) => {
       if(step === 1) {
         handleDeliverySubmit();
         setIsProcessing(false);
+        return;
+      }
+      if(step === 2) {
+        childCompRef.current.showAlert();
+        setIsProcessing(false)
         return;
       }
       setIsProcessing(false)
@@ -278,7 +283,7 @@ const handleCheckboxChange = (target) => {
             </CSSTransition>
             <CSSTransition delay={500} in={step === 2} classNames="a_c_d_b_transition_2" timeout={1000} unmountOnExit>
               <Elements stripe={stripePromise}>
-                  <CheckoutPayment/>
+                  <CheckoutPayment ref={childCompRef}/>
               </Elements>
             </CSSTransition>
           </div>
@@ -339,110 +344,110 @@ const VisualizeButton = styled(LoadingButton)({
 })
 
 
-function CheckoutPayment() {
+const CheckoutPayment = forwardRef((props, ref) => {
   const [payment_message, setPaymentMessage] = useState(null);
-  
+  const router = useRouter();
+
   //hooks
   const stripe = useStripe();
   const elements = useElements();
-
-  const handleSubmit = async(e) => {
-      e.preventDefault();
-
+  
+  useImperativeHandle(ref, () => ({
+    async showAlert() {
       //fetch payment_intent_id from cookies
       let { payment_intent_id, customer_id } = parseCookies();
-
-      //if no existing payment_intent
       if(!payment_intent_id) {
-          try {
-              console.log('here')
-              //create a new customer on stripe
-              if(!customer_id) {
-                  const id = await createStripeCustomer();
-                  customer_id = id;
-              }
+        try {
+            console.log('here')
+            //create a new customer on stripe
+            if(!customer_id) {
+                const id = await createStripeCustomer();
+                customer_id = id;
+            }
 
-              //store customer_id in cookies
-              setCookie(null, 'customer_id', customer_id, {
-                  maxAge: 30 * 24 * 60 * 60,
-                  path: '/',
-              });
+            //store customer_id in cookies
+            setCookie(null, 'customer_id', customer_id, {
+                maxAge: 30 * 24 * 60 * 60,
+                path: '/',
+            });
 
-              //append invoice items to customer
-              await appendInvoiceItems(customer_id);
+            //append invoice items to customer
+            await appendInvoiceItems(customer_id);
 
-              //append shipping costs to user
-              await calculateShipping(customer_id);
+            //append shipping costs to user
+            await calculateShipping(customer_id);
 
-              //create invoice on customer
-              const invoice_id = await createStripeInvoice(customer_id);
+            //create invoice on customer
+            const invoice_id = await createStripeInvoice(customer_id);
 
-              //store invoice_id in cookies
-              setCookie(null, 'invoice_id', invoice_id, {
-                  maxAge: 30 * 24 * 60 * 60,
-                  path: '/',
-              });
-              
-              //finalize invoice
-              const finalized_invoice = await finalizeStripeInvoice(invoice_id);
+            //store invoice_id in cookies
+            setCookie(null, 'invoice_id', invoice_id, {
+                maxAge: 30 * 24 * 60 * 60,
+                path: '/',
+            });
+            
+            //finalize invoice
+            const finalized_invoice = await finalizeStripeInvoice(invoice_id);
 
-              //update payment_intent_id
-              payment_intent_id = finalized_invoice.payment_intent;
+            //update payment_intent_id
+            payment_intent_id = finalized_invoice.payment_intent;
 
-              //store payment_intent_id in cookies
-              setCookie(null, 'payment_intent_id', payment_intent_id, {
-                  maxAge: 30 * 24 * 60 * 60,
-                  path: '/',
-              });
+            //store payment_intent_id in cookies
+            setCookie(null, 'payment_intent_id', payment_intent_id, {
+                maxAge: 30 * 24 * 60 * 60,
+                path: '/',
+            });
 
-          } catch (err) {
-              //display errors
-              setPaymentMessage({msgBody: err.message, msgError: true});
-              return;
-          }
-      }
+        } catch (err) {
+            //display errors
+            setPaymentMessage({msgBody: err.message, msgError: true});
+            return;
+        }
+    }
 
-      try {
-          //fetch payment intent
-          const payment_intent = await fetchPaymentIntent(payment_intent_id);
+    try {
+        //fetch payment intent
+        const payment_intent = await fetchPaymentIntent(payment_intent_id);
 
-          //confirm card payment
-          const { status } = await confirmCardPayment(payment_intent, elements, stripe);
+        //confirm card payment
+        const { status } = await confirmCardPayment(payment_intent, elements, stripe);
 
-          //on successful payment
-          if(status === "succeeded") {
-              //display success message
-              setPaymentMessage({msgBody: "Payment Successfully processed !", msgError: false});
+        //on successful payment
+        if(status === "succeeded") {
+            //display success message
+            setPaymentMessage({msgBody: "Payment Successfully processed !", msgError: false});
 
 
-              //clear cookies
-              destroyCookie(null, 'payment_intent_id');
-              destroyCookie(null, 'customer_id');
-              destroyCookie(null, 'invoice_id');
+            //clear cookies
+            destroyCookie(null, 'payment_intent_id');
+            destroyCookie(null, 'customer_id');
+            destroyCookie(null, 'invoice_id');
 
-              // clear local storage
-              checkoutStorage.removeItem('billing_details');
-              checkoutStorage.removeItem('delivery_preference');
+            // clear local storage
+            checkoutStorage.removeItem('billing_details');
+            checkoutStorage.removeItem('delivery_preference');
 
-              //redirect to success page
-              console.log("DONE");
-          }
+            //redirect to success page
+            console.log("DONE");
+            router.push("/success");
+        }
 
-          //on fail payment
-      } catch (err) {
-          //display errors
-          setPaymentMessage({msgBody: err.message, msgError: true});
-          return;
-      }
-  }
+        //on fail payment
+    } catch (err) {
+        //display errors
+        setPaymentMessage({msgBody: err.message, msgError: true});
+        return;
+    }
+    },
+  }));
+  
   return (
-      <form onSubmit={handleSubmit} style={{width: 250}}>
+      <form style={{width: 250}}>
           <CardElement/>
-          <button type="submit" disabled={!stripe}>Submit Order</button>
           {payment_message && <p>{payment_message.msgBody}</p>}
       </form>
   )
-}
+});
 
 const confirmCardPayment = async(payment_intent, elements, stripe) => {
   const {error, paymentIntent} = await stripe.confirmCardPayment(payment_intent.client_secret, {
